f2cbdcccbbbd2b15305137cb6f326da8ce2005cc
manuelguerra1 <chingo2005@gmail.com>
2023-01-12 18:33:55 -0300

    agregando hogas de estilo

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+// app.get('/register', (req,res)=>{
+//     res.sendFile(__dirname + '/views/register.html');
+// });
+// app.get('/login', (req,res)=>{
+//     res.sendFile(__dirname + '/views/login.html');
+// });
