10fafc79eeabfa8cfb9f25874410e0ba06841bb4
DiegooNP <diegopaez73@gmail>
2023-02-20 16:06:42 -0300

    Funcionalidad de creacion de usuario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,14 +1,65 @@
 const path = require('path');
+const fs = require('fs');
+
+const usersPath = path.join(__dirname, '/../data/users.json');
 
 const usersController = {
+    getUsers: ()=>{
+        return JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
+    },
     login: function (req, res) {
         res.render('login')
     },
     
     register: function (req, res) {
         res.render('register')
-    } 
+    },
+
+    usersCreate:function (req, res) {
+        res.render('../products/register')
+    },
+
+    usersStore:function (req,res) {
+        let user = usersController.getUsers();
+        
+        let newUser = {            
+            "id":req.body.id,
+            "name": req.body.name,
+            "lastname":req.body.lastname,
+            "email":req.body.email,
+            "username":req.body.username,
+            "password":req.body.password,
+            "confirmpassword": req.body.confirmpassword
+    }
+
+    console.log("nuevo usuario", newUser);
+
+user.push(newUser);
+
+    fs.writeFileSync(usersPath, JSON.stringify(user, null, ' '));
+
+    res.redirect('register');
+
+
+    },
+
+    usersEdit:{
+
+    },
+    update:{
+
+    },
+
+    delete:{
+
+    },
+
+    destroy:{
+
+    }
+
 
 }
+
 
 module.exports = usersController;
--- src/routers/users.js
+++ src/routers/users.js
@@ -6,4 +6,15 @@ router.get('/login', usersController.login);
 
 router.get('/register', usersController.register);
 
+// Crear
+router.get ('/userCreate', usersController.usersCreate);
+router.post('/userSave', usersController.usersStore);
+// // Editar
+// router.get ('/useredit/:id', usersController.usersEdit);
+// router.put ('/usermodify/:id', usersController.usersUpdate);
+// // Eliminar
+// router.get ('/userdelete/:id', usersController.usersDelete);
+// router.delete ('/usereliminate/:id', usersController.usersDestroy);
+
+
 module.exports = router;
