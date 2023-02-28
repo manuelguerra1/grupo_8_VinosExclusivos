7411c2c86f9f110d39ccca44b0fc76f7c4206252
liliandevico <liliandevico@gmail.com>
2023-02-28 16:48:39 -0300

    creamos el userController y modificamos las rutas de user

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -5,14 +5,14 @@ const path = require('path');
 const productsPath = path.join(__dirname, '/../data/products.json');
 
 const productsController = {
-    getProducts: ()=>{
+    getProducts: () => {
         return JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
     },
 
-    
+
     allProduct: function (req, res) {
         res.render('./products/allProduct', {
-        productList: productsController.getProducts()
+            productList: productsController.getProducts()
         });
     },
 
@@ -20,7 +20,7 @@ const productsController = {
         let productId = req.params.id;
         let producto = productsController.getProducts().find(producto => producto.id == productId);
 
-        res.render('./products/productDetail',{
+        res.render('./products/productDetail', {
             product: producto
         });
     },
@@ -29,11 +29,11 @@ const productsController = {
         res.render('./products/productCreateForm')
     },
 
-    store: (req,res)=>{
+    store: (req, res) => {
 
         let product = productsController.getProducts();
         // let image = [];
-        
+
         // if (req.files) {
         //     req.files.forEach(file =>{
         //         image.push({
@@ -41,111 +41,111 @@ const productsController = {
         //             "name": file.filename,
         //         });
         //     });
-    
+
         // } else{
         //     image.push("default.jpg");
         // } 
 
         let newProducts = {
-                "id":Date.now(),
-                "name": req.body.name,
-                "description":req.body.description,
-                "price":req.body.price,
-                "varietal":req.body.price,
-                "year":req.body.year,
-                "origen":req.body.origen,
-                "region":req.body.region,
-                "category":req.body.category,
-                "available": true,
-                "image": req.file.filename
-                
+            "id": Date.now(),
+            "name": req.body.name,
+            "description": req.body.description,
+            "price": req.body.price,
+            "varietal": req.body.price,
+            "year": req.body.year,
+            "origen": req.body.origen,
+            "region": req.body.region,
+            "category": req.body.category,
+            "available": true,
+            "image": req.file.filename
+
         }
 
         // console.log("nuevo producto", newProducts);
-    
+
         product.push(newProducts);
-    
+
         fs.writeFileSync(productsPath, JSON.stringify(product, null, ' '));
-    
+
         res.redirect('/');
     },
 
     productEdit: function (req, res) {
         let productId = req.params.id;
         let producto = productsController.getProducts().find(producto => producto.id == productId);
-        
-        
+
+
         return res.render('./products/productEditForm', {
             product: producto,
             id: req.params.id
         });
-    }, 
-    
-    update: (req,res) =>{
+    },
+
+    update: (req, res) => {
         let productId = req.params.id;
         let productos = productsController.getProducts();
-    
-        productos.forEach((producto, index) =>{
-            if(producto.id == productId){
-            producto.name = req.body.name,
-            producto.id= req.body.id,
-            producto.description= req.body.description,
-            producto.price= req.body.price,
-            producto.varietal= req.body.varietal,
-            producto.year= req.body.year,
-            producto.origen= req.body.origen,
-            producto.region= req.body.region,
-            producto.category= req.body.category,
-            //ponemos un ternario para que si me trae una imagen cambie el valor o foto y sino queda la misma imagen
-            producto.image=  req.file ? req.file.filename : producto.image
-            
 
-            productos[index] = producto;
-        }
-    });
-    
+        productos.forEach((producto, index) => {
+            if (producto.id == productId) {
+                producto.name = req.body.name,
+                    producto.id = req.body.id,
+                    producto.description = req.body.description,
+                    producto.price = req.body.price,
+                    producto.varietal = req.body.varietal,
+                    producto.year = req.body.year,
+                    producto.origen = req.body.origen,
+                    producto.region = req.body.region,
+                    producto.category = req.body.category,
+                    //ponemos un ternario para que si me trae una imagen cambie el valor o foto y sino queda la misma imagen
+                    producto.image = req.file ? req.file.filename : producto.image
+
+
+                productos[index] = producto;
+            }
+        });
+
         fs.writeFileSync(productsPath, JSON.stringify(productos, null, ' '));
-    
+
         res.redirect('/');
-    
+
     },
-    
-    delete: (req,res) => {
+
+    delete: (req, res) => {
         let productId = req.params.id;
         let product = productsController.getProducts().find(product => product.id == productId);
-    
-        res.render ('./products/productDeleteForm', {
+
+        res.render('./products/productDeleteForm', {
             title: 'Eliminar producto',
             product: product
         });
-    
+
     },
-    
+
     destroy: (req, res) => {
         let productId = req.params.id;
-        let products= productsController.getProducts();
-       
+        let products = productsController.getProducts();
+
         // NO FUNCIONA 
         // products = products.filter(product => product.id != productId);
-        
+
         // ENCUENTRA EL PRODUCTO
         let product = products.find(product => product.id == productId)
         //ENCUENTRA EL INDICE DEL PRODUCTO
         let productIndex = products.indexOf(product)
-        
+
         // ELIMINA EL PRODUCTO QUE COINCIDE CON EL INDICE DEL PRODUCTO
         products.splice(productIndex, 1)
-        
+
         // PREGUNTA SI TIENE IMAGEN CUANDO ELIMINA UN PRODUCTO
         // SI TIENE UNA IMAGEN, LA BORRA DE LA CARPETA IMGS
         fs.unlinkSync(__dirname, `../../public/img/product/${product.image}`)
-        
-        
-        fs.writeFileSync (productsPath, JSON.stringify(products, null, ' '));
-    
+
+
+        fs.writeFileSync(productsPath, JSON.stringify(products, null, ' '));
+
         res.redirect('/');
     }
-    
+
 }
 
 module.exports = productsController;
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -4,46 +4,47 @@ const fs = require('fs');
 const usersPath = path.join(__dirname, '/../data/users.json');
 
 const usersController = {
-    getUsers: ()=>{
+    getUsers: () => {
         return JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
     },
     login: function (req, res) {
         res.render('./users/login')
     },
-    
+
     register: function (req, res) {
         res.render('./users/register')
     },
 
-    usersCreate:function (req, res) {
+    usersCreate: function (req, res) {
         res.render('./users/register')
     },
 
-    usersStore:function (req,res) {
+    usersStore: function (req, res) {
         let user = usersController.getUsers();
-        
-        let newUser = {            
-            "id":req.body.id,
+
+        let newUser = {
+            "id": req.body.id,
             "name": req.body.name,
-            "lastname":req.body.lastname,
-            "email":req.body.email,
-            "username":req.body.username,
-            "password":req.body.password,
+            "lastname": req.body.lastname,
+            "email": req.body.email,
+            "username": req.body.username,
+            "password": req.body.password,
             "confirmpassword": req.body.confirmpassword
-    }
+        }
 
-    console.log("nuevo usuario", newUser);
 
-user.push(newUser);
+        // console.log("nuevo usuario", newUser);
 
-    fs.writeFileSync(usersPath, JSON.stringify(user, null, ' '));
+        user.push(newUser);
 
-    res.redirect('register');
+        fs.writeFileSync(usersPath, JSON.stringify(user, null, ' '));
+
+        res.redirect('register');
 
 
     },
 
-    usersEdit:{ function (req, res){
+    usersEdit: function (req, res) {
         let userId = req.params.id;
         let usuario = usersController.getUsers().find(usuario => usuario.id == userId);
 
@@ -53,20 +54,68 @@ user.push(newUser);
         });
 
     },
-    update:{
+
+    update: (req, res) => {
+        let userId = req.params.id;
+        let usuarios = usersController.getUsers();
+
+        usuarios.forEach((usuario, index) => {
+            if (usuario.id == userId) {
+                usuario.name = req.body.name,
+                    usuario.lastname = req.body.lastname,
+                    usuario.email = req.body.email,
+                    usuario.username = req.body.username,
+                    usuario.password = req.body.password,
+                    usuario.confirmpassword = req.body.confirmpassword,
+                    usuario.image = req.body.image,
+
+                    //ponemos un ternario para que si me trae una imagen cambie el valor o foto y sino queda la misma imagen
+                    usuario.image = req.file ? req.file.filename : usuario.image
+
+                usuarios[index] = usuario;
+            }
+
+        });
+        fs.writeFileSync(usersPath, JSON.stringify(usuarios, null, ' '));
+
+        res.redirect('/');
 
     },
 
-    delete:{
+    delete: (req, res) => {
+        let userId = req.params.id;
+        let user = usersController.getUsers().find(user => user.id == userId);
+
+        res.render('./users/userEditForm', {
+            title: 'Eliminar usuario',
+            user: user
+        });
 
     },
 
-    destroy:{
+destroy:(req, res) => {
+    let userId = req.params.id;
+    let users = usersController.getUsers();
+ 
+    // ENCUENTRA EL USUARIO
+ let user = users.find(user => user.id == userId)
+ 
+ //ENCUENTRA EL INDICE DEL USUARIO
+ let userIndex = users.indexOf(user)
 
-    }
+ // ELIMINA EL USUARIO QUE COINCIDE CON EL INDICE DEL USUARIO
+ users.splice(userIndex, 1)
+
+ // PREGUNTA SI TIENE IMAGEN CUANDO ELIMINA UN USUARIO
+ // SI TIENE UNA IMAGEN, LA BORRA DE LA CARPETA IMGS
+ fs.unlinkSync(__dirname, `../../public/img/users/${user.image}`)
 
 
+ fs.writeFileSync(usersPath, JSON.stringify(users, null, ' '));
+
+ res.redirect('/');
 }
 
+}
 
 module.exports = usersController;
