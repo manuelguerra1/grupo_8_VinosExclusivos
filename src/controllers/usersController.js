1084819bc5375be4f9fc471d9d90b806f86755ac
DiegooNP <diegopaez73@gmail>
2023-02-28 19:00:07 -0300

    Terminamos commit anterior

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,121 +1,114 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const usersPath = path.join(__dirname, '/../data/users.json');
+const usersPath = path.join(__dirname, "/../data/users.json");
 
 const usersController = {
-    getUsers: () => {
-        return JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
-    },
-    login: function (req, res) {
-        res.render('./users/login')
-    },
+  getUsers: () => {
+    return JSON.parse(fs.readFileSync(usersPath, "utf-8"));
+  },
+  login: function (req, res) {
+    res.render("./users/login");
+  },
 
-    register: function (req, res) {
-        res.render('./users/register')
-    },
+  register: function (req, res) {
+    res.render("./users/register");
+  },
 
-    usersCreate: function (req, res) {
-        res.render('./users/register')
-    },
+  usersCreate: function (req, res) {
+    res.render("./users/register");
+  },
 
-    usersStore: function (req, res) {
-        let user = usersController.getUsers();
+  usersStore: function (req, res) {
+    let user = usersController.getUsers();
 
-        let newUser = {
-            "id": req.body.id,
-            "name": req.body.name,
-            "lastname": req.body.lastname,
-            "email": req.body.email,
-            "username": req.body.username,
-            "password": req.body.password,
-            "confirmpassword": req.body.confirmpassword
-        }
+    let newUsers = {
+      id: req.body.id,
+      name: req.body.name,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      confirmpassword: req.body.confirmpassword,
+    };
 
+    // console.log("nuevo usuario", newUser);
 
-        // console.log("nuevo usuario", newUser);
+    user.push(newUsers);
 
-        user.push(newUser);
+    fs.writeFileSync(usersPath, JSON.stringify(user, null, " "));
 
-        fs.writeFileSync(usersPath, JSON.stringify(user, null, ' '));
+    res.redirect("register");
+  },
 
-        res.redirect('register');
+  usersEdit: function (req, res) {
+    let userId = req.params.id;
+    let usuario = usersController
+      .getUsers()
+      .find((usuario) => usuario.id == userId);
 
+    return res.render("./users/userEditForm", {
+      user: usuario,
+      id: req.params.id,
+    });
+  },
 
-    },
+  usersUpdate: (req, res) => {
+    let userId = req.params.id;
+    let usuarios = usersController.getUsers();
 
-    usersEdit: function (req, res) {
-        let userId = req.params.id;
-        let usuario = usersController.getUsers().find(usuario => usuario.id == userId);
+    usuarios.forEach((usuario, index) => {
+      if (usuario.id == userId) {
+        (usuario.name = req.body.name),
+          (usuario.lastname = req.body.lastname),
+          (usuario.email = req.body.email),
+          (usuario.username = req.body.username),
+          (usuario.password = req.body.password),
+          (usuario.confirmpassword = req.body.confirmpassword),
+          // usuario.image = req.body.image,
 
-        return res.render('./users/userEditForm', {
-            user: usuario,
-            id: req.params.id
-        });
+          //ponemos un ternario para que si me trae una imagen cambie el valor o foto y sino queda la misma imagen
+          (usuario.image = req.file ? req.file.filename : usuario.image);
 
-    },
+        usuarios[index] = usuario;
+      }
+    });
+    fs.writeFileSync(usersPath, JSON.stringify(usuarios, null, " "));
 
-    update: (req, res) => {
-        let userId = req.params.id;
-        let usuarios = usersController.getUsers();
+    res.redirect("/");
+  },
 
-        usuarios.forEach((usuario, index) => {
-            if (usuario.id == userId) {
-                usuario.name = req.body.name,
-                    usuario.lastname = req.body.lastname,
-                    usuario.email = req.body.email,
-                    usuario.username = req.body.username,
-                    usuario.password = req.body.password,
-                    usuario.confirmpassword = req.body.confirmpassword,
-                    usuario.image = req.body.image,
+  usersDelete: (req, res) => {
+    let userId = req.params.id;
+    let user = usersController.getUsers().find((user) => user.id == userId);
 
-                    //ponemos un ternario para que si me trae una imagen cambie el valor o foto y sino queda la misma imagen
-                    usuario.image = req.file ? req.file.filename : usuario.image
+    res.render("./users/userEditForm", {
+      title: "Eliminar usuario",
+      user: user,
+    });
+  },
 
-                usuarios[index] = usuario;
-            }
-
-        });
-        fs.writeFileSync(usersPath, JSON.stringify(usuarios, null, ' '));
-
-        res.redirect('/');
-
-    },
-
-    delete: (req, res) => {
-        let userId = req.params.id;
-        let user = usersController.getUsers().find(user => user.id == userId);
-
-        res.render('./users/userEditForm', {
-            title: 'Eliminar usuario',
-            user: user
-        });
-
-    },
-
-destroy:(req, res) => {
+  usersDestroy: (req, res) => {
     let userId = req.params.id;
     let users = usersController.getUsers();
- 
+
     // ENCUENTRA EL USUARIO
- let user = users.find(user => user.id == userId)
- 
- //ENCUENTRA EL INDICE DEL USUARIO
- let userIndex = users.indexOf(user)
+    let user = users.find((user) => user.id == userId);
 
- // ELIMINA EL USUARIO QUE COINCIDE CON EL INDICE DEL USUARIO
- users.splice(userIndex, 1)
+    //ENCUENTRA EL INDICE DEL USUARIO
+    let userIndex = users.indexOf(user);
 
- // PREGUNTA SI TIENE IMAGEN CUANDO ELIMINA UN USUARIO
- // SI TIENE UNA IMAGEN, LA BORRA DE LA CARPETA IMGS
- fs.unlinkSync(__dirname, `../../public/img/users/${user.image}`)
+    // ELIMINA EL USUARIO QUE COINCIDE CON EL INDICE DEL USUARIO
+    users.splice(userIndex, 1);
 
+    // PREGUNTA SI TIENE IMAGEN CUANDO ELIMINA UN USUARIO
+    // SI TIENE UNA IMAGEN, LA BORRA DE LA CARPETA IMGS
+    fs.unlinkSync(__dirname, `../../public/img/users/${user.image}`);
 
- fs.writeFileSync(usersPath, JSON.stringify(users, null, ' '));
+    fs.writeFileSync(usersPath, JSON.stringify(users, null, " "));
 
- res.redirect('/');
-}
-
-}
+    res.redirect("/");
+  },
+};
 
 module.exports = usersController;
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usersController = require('../controllers/usersController');
+// TODO - const upload = require('../middlewares/multerMiddleware')
 const router = express.Router();
 
 router.get('/login', usersController.login);
@@ -7,16 +8,17 @@ router.get('/login', usersController.login);
 router.get('/register', usersController.register);
 
 // Crear
+//TODO Agregar en la linea 13 -  upload.single('image')
 router.get ('/userCreate', usersController.usersCreate);
 router.post('/userSave', usersController.usersStore);
 
 // // Editar
-router.get ('/useredit/:id', usersController.usersEdit);
-router.put ('/usermodify/:id', usersController.usersUpdate);
+router.get ('/userEdit/:id', usersController.usersEdit);
+router.put ('/userModify/:id', usersController.usersUpdate);
 
 // // Eliminar
-router.get ('/userdelete/:id', usersController.usersDelete);
-router.delete ('/usereliminate/:id', usersController.usersDestroy);
+router.get ('/userDelete/:id', usersController.usersDelete);
+router.delete ('/userEliminate/:id', usersController.usersDestroy);
 
 
 module.exports = router;
