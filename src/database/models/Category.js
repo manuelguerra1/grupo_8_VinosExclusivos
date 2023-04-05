c0f0075426e8d994d444364ca1f7efaf6479f9aa
Diego Paez <119956106+DiegooNP@users.noreply.github.com>
2023-04-05 16:56:41 -0300

    models categories

---
+++ src/database/models/Category.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = "Category";
+    let cols = {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      category_name: {
+        type: DataTypes.STRING(255),
+      },
+    };
+    let config = {
+      timestamps: true,
+      creaded_at: "created_at",
+      updated_at: "updated_at",
+      deleted_at: "deleted_at",
+      paranoid: true,
+    };
+  
+    const Category = sequelize.define(alias, cols, config);
+  
+    return Category;
+  };
