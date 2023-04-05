3c961859b754af15b148d36c84de0439ef55654e
Lilian Devico <119797440+liliandevico@users.noreply.github.com>
2023-04-05 17:10:51 -0300

    solucionando errores en modelos

---
+++ src/database/models/Cart.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = "Cart";
+    let cols = { 
+    id: {
+        type:DataTypes.INTEGER,
+        autoIncrement: true,
+    },
+    total: {
+        type:DataTypes.DECIMAL,
+    },
+
+    payment_id: {
+        type:DataTypes.INTEGER,
+    },
+
+    item_id: {
+        type:DataTypes.INTEGER,
+    },
+        user_id: {
+            type:DataTypes.INTEGER,
+    }
+};
+    
+    
+        let config = {
+          timestamps: true,
+          created_at: "created_at",
+          updated_at: "updated_at",
+          deleted_at: "deleted_at",
+          paranoid: true,
+        };
+      
+        const Cart = sequelize.define(alias, cols, config);
+      
+        return Cart;
+      };
--- src/database/models/Cart_product.js
+++ src/database/models/Cart_product.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     let alias = "Cart_product";
-    let cols ={
+    let cols = {
       id:{
         type:DataTypes.INTEGER,
         autoIncrement: true,
@@ -10,19 +10,17 @@ module.exports = (sequelize, DataTypes) => {
         type:DataTypes.INTEGER,
       },
       product_id:{
-        type:DataTypes,INTEGER,
+        type:DataTypes.INTEGER,
       },
       cart_id:{
         type:DataTypes.INTEGER,
-      },
-      created_at:{
-        type:DataTypes.timestamps
       }
+     
 
     };
     let config = {
       timestamps: true,
-      creaded_at: "created_at",
+      created_at: "created_at",
       updated_at: "updated_at",
       deleted_at: "deleted_at",
       paranoid: true,
