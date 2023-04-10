5f70dcec5bd41b521cfdfbb8b25c7f92a03a13e8
anrrid <rodriguez.antonella.n@gmail.com>
2023-04-09 21:18:49 -0300

    Edicion de base de datos, listar productos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,28 +3,38 @@ const sequelize = db.sequelize;
 const { Op } = require("sequelize");
 
 const mainController = {
-  getProducts: () => {
-    return JSON.parse(fs.readFileSync(productsPath, "utf-8"));
-  },
+  // getProducts: () => {
+  //   return JSON.parse(fs.readFileSync(productsPath, "utf-8"));
+  // },
 
   index: function (req, res) {
     // res.render('pages/index')
     // ENVIA TODOS LOS PRODUCTOS DEL JSON AL ARCHIVO INDEX
     res.render("index");
   },
-  prueba: async function (req, res) {
-    try {
-      // let producto = await db.Product.findAll();
-      let usuario = await db.User.findAll();
+  'prueba': (req, res) => {
+    db.User.findAll()
+        .then(users => {
+            res.json({users})
+        })
+}
+  // prueba: async function (req, res) {
+  //   try {
+  //     // let producto = await db.Product.findAll();
+  //     let usuario = await db.User.findAll(
+  //     {
+  //       attributes: ['id', 'name'],
+  //     });
 
-      res.json({
-        // producto,
-        usuario,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  },
+  //     res.json({
+  //       // producto,
+  //       success: true,
+  //       data: usuario,
+  //     });
+  //   } catch (error) {
+  //     res.json({ error });
+  //   }
+  // },
 };
 
 module.exports = mainController;
