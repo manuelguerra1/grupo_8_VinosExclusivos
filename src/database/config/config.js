524ea62af99dc3bd1a772b6ca9750790f12c9860
Diego Paez <119956106+DiegooNP@users.noreply.github.com>
2023-05-27 20:51:02 -0300

    agregando estilo a edicion de usuario

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports=
 {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "123456",
     "database": "db_FooDrinks",
     "host": "127.0.0.1",
     "dialect": "mysql"
