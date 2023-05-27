cb1694e95e86ffb9c40ffb4fed017eb4266157b0
anrrid <rodriguez.antonella.n@gmail.com>
2023-05-27 14:27:45 -0300

    .

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,8 +2,8 @@ module.exports=
 {
   "development": {
     "username": "root",
-    "password": "123456",
-    "database": null,
+    "password": null,
+    "database": "db_FooDrinks",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
